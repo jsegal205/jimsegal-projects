d44f6e3dc44ff29990546802dce58d115712f7e1
Jim Segal <hello@jimsegal.com>
2021-11-02 21:34:35 -0500

    remove unused references

--- src/pages/travel/index.js
+++ src/pages/travel/index.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import { apiUrlBase } from "../../utils";
 import useFetch from "../../utils/useFetch";
